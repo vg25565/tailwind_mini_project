4732d29a71fa98e79e2226a4f7363ae7788d4754
vg25565 <84933862+vg25565@users.noreply.github.com>
2024-07-17 18:20:44 +0530

    Add files via upload

---
+++ configTailwind.js
@@ -0,0 +1,13 @@
+tailwind.config={
+    darkMode: 'class',
+    theme :{
+        extend:{
+            colors:{
+                "logo":{
+                    700:'#1c59AE',
+                    800:'#114a9a',
+                }
+            }
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('knowMoreButton').addEventListener('click', function() {
+    document.body.classList.toggle('bg-black');
+    document.body.classList.toggle('text-white');
+});
